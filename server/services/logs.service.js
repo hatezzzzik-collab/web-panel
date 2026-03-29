e8bed372d5e0d54f8089f94b8ab8e439477ebbff
hatezzzzik-collab <hatezzzzik@gmail.com>
2026-03-29 08:26:00 +0600

    Delete server directory

--- server/services/logs.service.js
+++
@@ -1,9 +0,0 @@
-const { execCommand } = require('../utils/exec');
-
-const CONTAINER_NAME = process.env.CONTAINER_NAME || 'mtproxy';
-
-async function getLogs(tail = 100) {
-  return await execCommand(`docker logs ${CONTAINER_NAME} --tail ${tail} 2>&1`);
-}
-
-module.exports = { getLogs };
