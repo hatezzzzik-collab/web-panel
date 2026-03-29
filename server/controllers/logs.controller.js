824470d4bd0b05abdf4fd7ee363b28e32550a09d
hatezzzzik-collab <hatezzzzik@gmail.com>
2026-03-29 09:21:18 +0600

    Delete server directory

--- server/controllers/logs.controller.js
+++
@@ -1,3 +0,0 @@
-const svc = require('../services/logs.service');
-async function getLogs(req, res) { try { res.json({ ok: true, data: { logs: await svc.getLogs(Number(req.query.tail || 150)) } }); } catch (error) { res.status(500).json({ ok: false, error: String(error) }); } }
-module.exports = { getLogs };
