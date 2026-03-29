937c667a7857e56efe8780487b86065304081979
hatezzzzik-collab <hatezzzzik@gmail.com>
2026-03-29 09:04:33 +0600

    Delete server directory

--- server/routes/logs.routes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/logs.controller');
-const router = express.Router();
-router.get('/', controller.getLogs);
-module.exports = router;
